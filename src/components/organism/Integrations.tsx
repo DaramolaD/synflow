48181ebb75b601743220c14b1e8453df937163bd
Daniel <daniel.daramola@localtechnocrat.com>
2024-08-06 13:39:14 +0100

    Modify the style for integration section

--- src/components/organism/Integrations.tsx
+++ src/components/organism/Integrations.tsx
@@ -6,21 +6,26 @@ import integration from "@/assets/integration.svg";
 const Integrations = () => {
   return (
     <div className="sectionContainer flex flex-col gap-6 px-5 md:px-10 py-5">
-      <IntegrationNote />
-      <div className="grid gap-6">
-        <div className="grid gap-2">
-          <h1 className="text-2xl">
+      <div className="flex flex-col gap-6 lg:flex-row items-center">
+        <div className="flex flex-col gap-2">
+          <IntegrationNote />
+          <h1 className="text-2xl md:text-4xl">
             Integrate with your{" "}
             <span className="text-primary-400">Existing</span> tech Stack and
             workflows
           </h1>
-          <p className="text-muted-10 text-sm">
+          <p className="text-muted-10 text-sm md:text-lg">
             Connect your tools effortlessly, optimize your workflow, and
             streamline your data flow and enhance functionality
           </p>
         </div>
-        <div className="relative flex w-full items-center justify-center">
-          <Image fill alt="integration" src={integration} />
+        <div className="relative flex w-full max-w-full min-h-96 h-auto items-center justify-center">
+          <Image
+            fill
+            alt="integration"
+            src={integration}
+            className="w-full h-full"
+          />
         </div>
       </div>
     </div>
